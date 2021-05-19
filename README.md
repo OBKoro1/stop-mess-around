e22f0a0acd1f3330534eb287fda676158597de1c
OBKoro1 <1677593011@qq.com>
2021-05-19 16:05:01 +0800

    chore: init

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# stop-mess-around
+# stop-mess-around(停止浪费时间)
 
 Chrome停止摸鱼插件，停止下意识的摸鱼，自动关闭摸鱼网站，滚去学习，滚去做对未来有益的事情！
 
---
+++ deploy.sh
@@ -0,0 +1,10 @@
+
+# tag
+# https://git-scm.com/book/zh/v2/Git-%E5%9F%BA%E7%A1%80-%E6%89%93%E6%A0%87%E7%AD%BE
+
+# git tag -a 'v0.0.12' -m 'tag信息'
+# git push origin --tags
+# git tag
+
+# git tag -l | xargs git tag -d #删除所有本地分支
+# git fetch origin --prune #从远程拉取所有信息
