f6178a1307bb88e9c1e0541355b5543fa3a5e744
OBKoro1 <1677593011@qq.com>
2021-06-20 18:12:11 +0800

    feat: 插入到页面的js

---
+++ src/content/App.vue
@@ -0,0 +1,131 @@
+<!--
+ * Author       : OBKoro1
+ * Date         : 2021-06-15 13:51:30
+ * LastEditors  : OBKoro1
+ * LastEditTime : 2021-06-20 17:26:09
+ * FilePath     : /stop-mess-around/src/content/app.vue
+ * Description  : content 插入到页面的数据
+ * koroFileheader插件
+ * Copyright (c) ${now_year} by OBKoro1, All Rights Reserved.
+-->
+<template>
+  <div class="content-div">
+    <el-dialog :title="info.title"
+               :lock-scroll="true"
+               :visible.sync="dialogVisible"
+               width="30%"
+               :show-close="false"
+               :close-on-click-modal="false">
+      <span class="tip-info"
+            v-html="info.tip"></span>
+      <span slot="footer"
+            class="dialog-footer">
+        <el-button type="primary"
+                   @click="handleClose">{{ info.confirmBtn }}</el-button>
+      </span>
+    </el-dialog>
+  </div>
+</template>
+
+<script>
+import { utils } from '../utils/index'
+import { defaultSetting } from '../utils/Default'
+import NET from '../utils/net'
+
+export default {
+  data() {
+    return {
+      Setting: null,
+      dialogVisible: false,
+      item: null,
+      index: 0,
+      info: {
+        title: '',
+        tip: '',
+        confirmBtn: '',
+      },
+    }
+  },
+  mounted() {
+    console.log('stop-mess-around 插入')
+    this.run()
+    // 检测url变更
+    setInterval(this.run, 6000)
+  },
+  methods: {
+    // 检测链接
+    async run() {
+      this.Setting = (await utils.getChromeStorage(NET.GLOBALSETTING)) || defaultSetting
+      const tableData = (await utils.getChromeStorage(NET.TABLELIST)) || []
+      const isMatch = utils.checkUrl(tableData, window.location.href)
+      if (!isMatch) return
+      // 不重复展示关闭提示 已经关闭 或者已经开启的 不再重复出现
+      // 如果切换了 则可以展示弹窗
+      if (this.item !== null && this.item.open === isMatch.item.open) return
+      this.item = isMatch.item // 匹配到的选项
+      this.index = isMatch.index // 匹配到的index
+      this.matchHandle()
+    },
+
+    /**
+     * @description 获取随机提示信息
+     * @param * typeName 数组名字
+     * @return {*}
+     */
+    getRandomTip(typeName) {
+      const len = this.Setting[typeName].length
+      const count = Math.floor(Math.random() * len)
+      return `${this.Setting[typeName][count]}`
+    },
+
+    /**
+     * @description 获取提示信息
+     * @param * item
+     */
+    getTip(item) {
+      if (item.tip) {
+        // 使用item的tip
+        this.info.tip = item.tip
+      } else {
+        this.info.tip = this.getRandomTip('tipArr')
+      }
+      this.info.title = this.getRandomTip('titleArr')
+      this.info.confirmBtn = this.getRandomTip('confirmArr')
+    },
+
+    /**
+     * @description 匹配后显示提示消息以及关闭页面
+     * @param * item 匹配到的对象
+     * @return {*}
+     */
+    matchHandle() {
+      if (!this.item.open) {
+        this.$message(`${this.item.labelName}-摸鱼网站已关闭`)
+        return
+      }
+      this.getTip(this.item)
+      this.dialogVisible = true
+    },
+    // 关闭弹窗
+    handleClose() {
+      this.dialogVisible = false // 先关闭弹窗
+      setTimeout(() => {
+        if (this.item.jump) {
+          // 跳转
+          window.location.href = this.item.jumpUrl
+        } else {
+          // 关闭
+          chrome.extension.sendRequest({ message: 'close-tab' })
+        }
+      }, this.item.time)
+    },
+  },
+}
+</script>
+
+<style scoped>
+.tip-info {
+  font-size: 16px;
+  line-height: 18px;
+}
+</style>
--- src/content/index.js
+++ src/content/index.js
@@ -1,26 +1,26 @@
-import { Message } from 'element-ui'
+/*
+ * Author       : OBKoro1
+ * Date         : 2021-05-17 16:17:59
+ * LastEditors  : OBKoro1
+ * LastEditTime : 2021-06-15 14:08:44
+ * FilePath     : /stop-mess-around/src/content/index.js
+ * Description  : content插入到页面的js
+ * koroFileheader插件
+ * Copyright (c) ${now_year} by OBKoro1, All Rights Reserved.
+ */
+import Vue from 'vue'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import AppComponent from './App.vue'
 
-// 通过Chrome插件的API加载字体文件
-(function insertElementIcons() {
-  const elementIcons = document.createElement('style')
-  elementIcons.type = 'text/css'
-  elementIcons.textContent = `
-        @font-face {
-            font-family: "element-icons";
-            src: url('${window.chrome.extension.getURL(
-    'fonts/element-icons.woff',
-  )}') format('woff'),
-            url('${window.chrome.extension.getURL(
-    'fonts/element-icons.ttf ',
-  )}') format('truetype'); /* chrome, firefox, opera, Safari, Android, iOS 4.2+*/
-        }
-    `
-  document.head.appendChild(elementIcons)
-}())
+const id = `app_vue_${Date.now()}`
+const root = document.createElement('div')
+root.id = id
+document.body.appendChild(root)
+Vue.use(ElementUI)
 
-console.log('chrome', chrome)
-
-Message({
-  type: 'info',
-  message: `stop-mess-around: ${chrome.i18n.getMessage('helloWorld')}`,
+// eslint-disable-next-line no-new
+new Vue({
+  el: `#${id}`,
+  render: (h) => h(AppComponent),
 })
