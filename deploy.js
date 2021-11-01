e2303d72b35f8593e8fe578f16a07baab273c351
OBKoro1 <1677593011@qq.com>
2021-11-01 13:31:05 +0800

    fix: 修复content无法加载外部字体文件的问题

--- deploy.js
+++ deploy.js
@@ -2,8 +2,8 @@
  * Author       : OBKoro1
  * Date         : 2021-06-18 11:08:23
  * LastEditors  : OBKoro1
- * LastEditTime : 2021-07-30 01:50:19
- * FilePath     : deploy.js
+ * LastEditTime : 2021-11-01 13:01:26
+ * FilePath     : /stop-mess-around/deploy.js
  * Description  : 同步package.json的配置到manifest.json中
  * koroFileheader插件
  * Copyright (c) ${now_year} by OBKoro1, All Rights Reserved.
@@ -12,25 +12,48 @@
 const path = require('path')
 const fs = require('fs')
 
-// 插件版本与描述
-const VERSION = process.env.npm_package_version
-let DESCRIPTION = '防摸鱼插件: 通过强制的手段禁止大家浪费时间摸鱼：在上班/学习期间下意识的打开摸鱼网站, 自动检测摸鱼网站, 提示激励信息后, 关闭摸鱼网站, 滚去学习，滚去做对未来有益的事情！'
-let PLUGINNAME = 'stop-mess-around'
+run()
 
-const { argv } = process
-const env = argv[2]
-
-if (env !== 'production') {
-  PLUGINNAME = 'stop-mess-around-file'
-  DESCRIPTION = `file: ${DESCRIPTION}`
-}
-const templateOption = {
-  DESCRIPTION,
-  VERSION,
-  PLUGINNAME,
+function run() {
+  changeVersion()
+  elementFont()
 }
 
-updateFillBuilderYAML(templateOption)
+// 修改element的引用地址 使element icon在其他网站上可用
+function elementFont() {
+  const elementPath = path.resolve(__dirname, './node_modules/element-ui/lib/theme-chalk/', 'index.css')
+  let content = fs.readFileSync(elementPath).toString()
+  // 替换匹配到的每个变量
+  // const reg = new RegExp('(fonts/element-icons.', 'g')
+  const reg = /\(fonts\/element-icons\./g
+
+  content = content.replace(reg, '(chrome-extension://__MSG_@@extension_id__/fonts/element-icons.')
+  // console.log('content', content)
+  fs.writeFileSync(elementPath, content)
+  console.log('run', elementPath, reg)
+}
+
+// 更改插件版本与描述
+function changeVersion() {
+  const VERSION = process.env.npm_package_version
+  let DESCRIPTION = '防摸鱼插件: 通过强制的手段禁止大家浪费时间摸鱼：在上班/学习期间下意识的打开摸鱼网站, 自动检测摸鱼网站, 提示激励信息后, 关闭摸鱼网站, 滚去学习，滚去做对未来有益的事情！'
+  let PLUGINNAME = 'stop-mess-around'
+
+  const { argv } = process
+  const env = argv[2]
+
+  if (env !== 'production') {
+    PLUGINNAME = 'stop-mess-around-file'
+    DESCRIPTION = `file: ${DESCRIPTION}`
+  }
+  const templateOption = {
+    DESCRIPTION,
+    VERSION,
+    PLUGINNAME,
+  }
+
+  updateFillBuilderYAML(templateOption)
+}
 
 // 修改manifest.template.json配置，替换manifest.production.json
 function updateFillBuilderYAML(option) {
